dc80a0cd8b9c20630296e8aef0ab50a5d6e6401d
Nickolas Burr <nickolasburr@gmail.com>
2019-07-29 21:09:11 -0400

    Add registration.php, polyfill.php

---
+++ registration.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * registration.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/polyfill/LICENSE.txt
+ *
+ * @package       AuroraExtensions_Polyfill
+ * @copyright     Copyright (C) 2019 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'AuroraExtensions_Polyfill',
+    __DIR__
+);
+
+require_once __DIR__ . '/src/polyfill.php';
